5b5af58cac499b982a5decc4f7e0be672ab74a29
Jawad606 <98688577+Jawad606@users.noreply.github.com>
2022-02-04 16:32:52 +0500

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Main extends Component {
+  render() {
+    return (
+      <>
+        <h3>My project </h3>
+        <p>I am very much exited to compte thsi task</p>
+        <p>Let install the bootstreap</p>
+      </>
+    );
+  }
+}
+export default Main;
